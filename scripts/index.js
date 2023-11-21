e24df1d96b4bbe475894de7ac99ff8ad7af58df9
Daniil Pavlov <daniilpavlovk@gmail.com>
2023-11-21 13:24:32 +0700

    3

--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,20 @@
 // @todo: Вывести карточки на страницу
 const cardsContainer = document.querySelector('.places__list');
 const cardTemplate = document.querySelector('#card-template').content;
+const cardData = initialCards;
 
-
-for (let i = 0; i < initialCards.length; i++) {
-  const cardElement = createCard(i);
+for (let cardDataz = 0; cardDataz < initialCards.length; cardDataz++) {
+  const cardElement = createCard(cardDataz);
   addCard(cardElement, cardsContainer);
 }
 
-function createCard(i) {
+function createCard(cardDataz) {
   const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
-  cardElement.querySelector('.card__image').src = initialCards[i].link;
-  cardElement.querySelector('.card__image').alt = initialCards[i].name;
-  cardElement.querySelector('.card__title').textContent = initialCards[i].name;
+  cardElement.querySelector('.card__image').src = cardData[cardDataz].link;
+  cardElement.querySelector('.card__image').alt = cardData[cardDataz].name;
+  cardElement.querySelector('.card__title').textContent = cardData[cardDataz].name;
 
-
-  
-  // @todo: Функция удаления карточки
   cardElement.querySelector('.card__delete-button').addEventListener('click', deleteCard);
-
-  function deleteCard() {
-    const cardToRemove = cardElement.closest('.places__item');
-    if (cardToRemove) {
-      cardToRemove.remove();
-    }
-  }
 
   return cardElement;
 }
@@ -32,3 +22,7 @@ function createCard(i) {
 function addCard(cardElement, cardsContainer) {
   cardsContainer.append(cardElement );
 } 
+
+function deleteCard(evt) {
+  evt.target.closest('.places__item').remove();
+}
